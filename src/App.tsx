f874d2c7f6000876d1a9d860f9ba7ef1c4357081
SeniorSam9 <rxrld050@hotmail.com>
2023-08-22 15:15:42 +0300

    further clean code

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Child from "./components/Child";
-import { ThemeContext } from "./contexts/ThemeContext";
+import { ThemeContext, useTheme } from "./contexts/ThemeContext";
 export default function App() {
-  const [isDarkTheme, setIsDarkTheme] = useState<boolean>(false);
-  function toggleDarkTheme() {
-    setIsDarkTheme((prev) => {
-      return !prev;
-    });
-  }
+  const { isDarkTheme, toggleDarkTheme } = useTheme();
 
   useEffect(() => {
     document.body.style.backgroundColor = isDarkTheme ? "#333" : "white";
     document.body.style.color = isDarkTheme ? "white" : "#333";
   }, [isDarkTheme]);
+
   return (
     <>
       <ThemeContext.Provider value={{ isDarkTheme, toggleDarkTheme }}>
--- src/contexts/ThemeContext.ts
+++ src/contexts/ThemeContext.ts
@@ -1,9 +1,21 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 interface Theme {
   isDarkTheme: boolean;
   toggleDarkTheme: Function;
 }
+
+export function useTheme() {
+  const [isDarkTheme, setIsDarkTheme] = useState<boolean>(false);
+
+  function toggleDarkTheme() {
+    setIsDarkTheme((prevTheme) => {
+      return !prevTheme;
+    });
+  }
+  return { isDarkTheme, toggleDarkTheme };
+}
+
 export const ThemeContext = createContext<Theme>({
   isDarkTheme: false,
   toggleDarkTheme: () => {},
